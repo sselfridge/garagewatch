0a8f20c13b4c230df3d577588b909a10c309807a
Sam Selfridge <Sam.Selfridge@gmail.com>
2020-04-06 18:39:37 -0700

    more logs

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/meinDB", { useNewUrlParser: true, useUnifiedTopology: true });
+console.log("Starting Connection");
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-  // we're connected!
+  console.log("// were connected!: ");
+
+  saveFluffy();
 });
+
 var kittySchema = new mongoose.Schema({
   name: String,
 });
-var Kitten = mongoose.model("Kitten", kittySchema);
 
-var silence = new Kitten({ name: "Silence" });
-
-var Kitten = mongoose.model("Kitten", kittySchema);
-var fluffy = new Kitten({ name: "fluffy" });
-fluffy.save(function (err, fluffy) {
-  if (err) return console.error(err);
-});
-
-console.log('And we"re done here');
+function saveFluffy() {
+  var Kitten = mongoose.model("Kitten", kittySchema);
+  var fluffy = new Kitten({ name: "fluffy" });
+  console.log("Saving Fluffy");
+  fluffy.save(function (err, fluffy) {
+    console.log("Fluffy Saved!");
+    if (err) return console.error(err);
+  });
+  console.log('And we"re done here');
+}
